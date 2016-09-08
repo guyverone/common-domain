d6e5ea02abdacadef64e068ae88526edd27a2cda
suhuade <huade@1wandian.com>
2016-09-08 19:56:39 +0800

    add exclusion type to @NotEmpty

--- src/main/java/common/domain/annotation/NotEmpty.java
+++ src/main/java/common/domain/annotation/NotEmpty.java
@@ -9,7 +9,8 @@ import java.lang.annotation.*;
  *
  * usage：
  * <br><tt>such as：@NotEmpty</tt> no specify parameter, then the value must be not empty.</br>
- * <br><tt>such as：@NotEmpty(EnumUsedBusinessType.TYPEA)</tt> only EnumUsedBusinessType.TYPEA type of Master needed to be validating.</br>
+ * <br><tt>such as：@NotEmpty(EnumUsedBusinessType.TYPEA)</tt> only EnumUsedBusinessType.TYPEA type of Object needed to be validating.</br>
+ * <br><tt>such as：@NotEmpty(exclusion={EnumUsedBusinessType.TYPEA})</tt> EnumUsedBusinessType.TYPEA type of Object won't be validating.</br>
  *
  * <p>Created by suhd on 2016-07-29.</p>
  */
@@ -18,4 +19,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotEmpty {
     EnumUsedBusinessType[] value() default EnumUsedBusinessType.EMPTY;
+    EnumUsedBusinessType[] exclusion() default EnumUsedBusinessType.EMPTY;
 }
